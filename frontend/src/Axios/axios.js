27ee657ccded90c8611d8aff34f90dfc675e658c
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2024-07-09 17:33:16 +0530

    Update axios.js

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
+    baseURL:"https://to-do-app-mern-backend.vercel.app/api"
 })
 export default instance
